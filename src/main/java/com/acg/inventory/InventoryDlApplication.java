9223f80907a82c1302bfa402a05572d8d649c212
Shagun Bakliwal <shagunbakliwal@gmail.com>
2018-09-16 20:52:49 +0530

    changed group id

---
+++ src/main/java/com/acg/inventory/InventoryDlApplication.java
@@ -0,0 +1,11 @@
+//package com.acg.inventory;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class InventoryDlApplication {
+//	public static void main(String[] args) {
+//		SpringApplication.run(InventoryDlApplication.class, args);
+//	}
+//}
